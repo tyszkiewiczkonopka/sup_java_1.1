836ba871540074a9828fbb2613df2b6ab388a4f4
Magdalena <tyszkiewicz.konopqa@gmail.com>
2023-10-05 11:03:32 +0200

    Second version of Guess Number game (with 'for' loop)

--- Main.java
+++ Main.java
@@ -2,34 +2,32 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final int MAX_TRIES = 5;
     public static void main(String[] args) {
         int numberToGuess = new Random().nextInt(100);
 
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Please choose an integer between 0 and 99");
-        int guessedNumber = scanner.nextInt();
-        int maxTries = 5;
-        int numberOfTries = 0;
+        Scanner scanner = new Scanner(System.in);
 
-        if (guessedNumber == numberToGuess) {
-            System.out.println("You guessed it!");
-        }
+        for (int maxTries = MAX_TRIES; maxTries > 0; maxTries--) {
+            int guessedNumber = scanner.nextInt();
 
-        while (numberToGuess != guessedNumber && maxTries > 1) {
-            maxTries--;
-            numberOfTries++;
+            if (numberToGuess == guessedNumber) {
+                System.out.println("You guessed it!");
+                break;
+            }
 
             if (guessedNumber > numberToGuess) {
                 System.out.println("Your number is GREATER than the one you are trying to guess");
             } else {
                 System.out.println("Your number is LOWER than the one you are trying to guess");
             }
-            System.out.println("Please try again - you've got " + maxTries + " more tries left");
-            guessedNumber = scanner.nextInt();
-        }
 
-        if (maxTries == 1) {
-            System.out.println("Sorry you didn't guess the number, the answer was: " + numberToGuess);
+            if (maxTries == 1) {
+                System.out.println("Sorry you didn't guess the number, the answer was: " + numberToGuess);
+            } else {
+                System.out.println("Please try again - you've got " + maxTries + " more tries left");
+            }
         }
 
     }
